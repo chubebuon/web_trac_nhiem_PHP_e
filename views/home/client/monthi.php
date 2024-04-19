6ced448b1ac0c533d69fee253d8374c7f3e6476f
“chubebuon” <23nguyenanhtuan@gmail.com>
2024-04-19 09:27:54 +0700

    commit

--- views/home/client/monthi.php
+++ views/home/client/monthi.php
@@ -1,98 +1,210 @@
-<!doctype html>
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <link rel="shortcut icon" href="./assets/image/icon.png" type="image/x-icon">
-  <title for="">Thi trắc nhiêm trực tuyến</title>
-  <link rel="stylesheet" href="./assets/style/index.css" type="text/css">
-  <link rel="stylesheet" href="./assets/style/monthi.css" type="text/css">
-  <script src="./assets/js/jquery-3.1.1.js"></script>
-  <script src="./assets/js/index.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"> -->
+    <title>Thi Trắc Nghiệm</title>
 </head>
-
-<body>
-  <div class="back_out">
-    <!--show background black-->
-  </div>
-  <div class="back_over">
-    <!--show background status image gif-->
-  </div>
-  <div class="main">
-    <div class="banner">
-      <!--Background image-->
+<body >
+<div class="container-fluid w-75 mt-2">
+    <nav>
+        <ul class="nav nav-underline justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link " href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link"  href="#">Bài Thi</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Đề Thi</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Hồ Sơ</a>
+        </li>
+        </ul>
+    </nav>
+ <header>
+    <div id="carouselExampleIndicators" class="carousel slide mt-3">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div id="loadajax" style="width:100%; height:auto;">
-      <div class="ttdk" style="height:40px; border:none; margin-bottom:1em;">
-        <p class="ttdktext" style="color:blue; margin-top:1em; float:left; width:20%;border-bottom:3px solid blue; margin-left:0.6em;">
-          Thông tin thí sinh</p>
-        <p class="ttdktext" style="margin-left:3.7em; width:65%; color:blue; margin-top:1em; border-bottom:3px solid blue;">
-          Danh sách môn thi</p>
-      </div>
-      <div class="body">
-        <div class='avatar'>
-          <div class='hellomem' style='width:100%; height:2.2em; background:seagreen;'>
-            <img src='assets/image/key.png' width='15' height='17' style='margin-top:0.5em;margin-left:0.5em; float:left;'>
-            <div class='xinchao' style='padding-top:0.5em;padding-left:0.5em;color:white; background-color:cadetblue; width:80%; height:1.7em; float:right;'>
-              XIN CHÀO</div>
-          </div>
-          <div class='imagemem' style='width:100%; height:10em;padding-top:0.6em;'>
-            <img src='<?= $target ?>' style='margin:auto; display:block; width:70%; height:96%;'>
-          </div>
-          <div class='chitiet' style=' width:100%; height:6.1em;'>
-            <div class='hoten' style='width:100%; height:3em;background:rgba(170,170,170,0.4);'>
-              <img src='assets/image/name.jpg' style='margin-top:0.5em;margin-left:0.3em; float:left;'>
-              <div style='font-size:13px;padding-top:0.5em;padding-left:0em;color:black;width:70%; height:1.7em; float:right;'>
-                <span>Họ và tên</span><br>
-                <span><b><?= $_SESSION['user_info']['hodem'] . $_SESSION['user_info']['ten'] ?></b></span>
-              </div>
-            </div>
-            <div class='sobd' style='width:100%; height:3.1em;background:rgba(170,170,170,0.4);'>
-              <img src='assets/image/ID.jpg' style='margin-top:0.5em;margin-left:0.3em; float:left;'>
-              <div style='font-size:13px;padding-top:0.5em;padding-left:0em;color:black;width:70%; height:1.7em; float:right;'>
-                <span>Số báo danh</span><br>
-                <span><b><?= $_SESSION['user_info']['sbd'] ?></b></span>
-              </div>
-            </div>
-            <div class='sobd' style='width:100%; height:3.1em;background:rgba(170,170,170,0.4);'>
-              <img src='assets/image/ID.jpg' style='margin-top:0.5em;margin-left:0.3em; float:left;'>
-              <div style='font-size:13px;padding-top:0.5em;padding-left:0em;color:black;width:70%; height:1.7em; float:right;'>
-                <span>Ngày sinh</span><br>
-                <span><b><?= $_SESSION['user_info']['ngaysinh'] ?></b></span>
-              </div>
-            </div>
-            <div class='sobd' style='width:100%; height:3.1em;background:rgba(170,170,170,0.4);'>
-              <img src='assets/image/ID.jpg' style='margin-top:0.5em;margin-left:0.3em; float:left;'>
-              <div style='font-size:13px;padding-top:0.5em;padding-left:0em;color:black;width:70%; height:1.7em; float:right;'>
-                <span>Nơi sinh</span><br>
-                <span><b><?= $_SESSION['user_info']['noisinh'] ?></b></span>
-              </div>
-            </div>
-          </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="https://caodang.fpt.edu.vn/wp-content/uploads/Banner1-1.png" class="d-block w-100" alt="...">
         </div>
-        <div class='dnh'>
-          <div class='loadModun' name='mmodun'>
-            <?php foreach ($moduns as $k => $v) : ?>
-              <div class='bomodun'>
-
-                <div>
-                  <img src='assets/image/small-list.png' width='25' height='26' style='float:left; margin-left:2em; margin-top:-0.3em;'>
-                  <p id='tench' style='margin-top:1em; margin-left:5.6em; cursor:pointer; padding-bottom:0.5em;'>
-                    <a href="index.php?controller=setupExam&ID=<?= $v->getTenmodun() ?>" id="tencha" style="text-decoration: none; "><?= $v->getTenmodun() ?></a>
-                  </p>
-                </div>
-
-
-              </div>
-            <?php endforeach; ?>
-          </div>
+        <div class="carousel-item">
+        <img src="https://caodang.fpt.edu.vn/wp-content/uploads/Banner-web-Chuyen-truong-Chuyen-hoc-phi-11.jpg" class="d-block w-100" alt="...">
         </div>
-        <p class="det" style="color: darkviolet; background: lavender;width: 62.5%;text-align: center; float: right; margin-right: 6em;margin-top: -1.1em;">
-          Thí sinh click vào tên môn thi để vào bài thi</p>
-      </div>
+        <div class="carousel-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-jOpy-CeNPrr2rEn1GMbPhqL5JgTnPsZE35SyqJQWpg&s" class="d-block w-100" alt="...">
+        </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+ </header>
+<article>
+<select class="form-select form-select-lg mb-3 mt-2" aria-label="Large select example">
+  <option selected>Khoá</option>
+  <option value="1">SP2024</option>
+  <option value="2">SMALL2024</option>
+  <option value="3">NEW2025</option>
+  <option value="3">MALL2025</option>
+  <option value="4">SP2025</option>
+</select>
+
+<select class="form-select form-select-sm" aria-label="Small select example">
+  <option selected>Tiến độ bài</option>
+  <option value="1">Sắp hết hạn</option>
+  <option value="2">Gần hết hạn</option>
+  <option value="3">Bài mới</option>
+</select>
+
+<select class="form-select form-select-sm mt-2" aria-label="Small select example">
+  <option selected>Bài đang làm</option>
+  <option value="1">SP2024</option>
+  <option value="2">NEW2024</option>
+  <option value="3">MG2024</option>
+</select>
+
+<div class="row d-flex">
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 20-04-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 90%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
   </div>
+</div>
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 25-04-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 70%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 30-04-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 60%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 10-05-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 35%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+</div>
+<div class="row d-flex">
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 25-05-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 25%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 20-06-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 15%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 25-06-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 13%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+<div class="card col mt-4 justify-content-between" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SP24_Hà Nội</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">1 Bài thi</h6>
+    <p class="card-text">Thời gian kết thúc: 30-06-2024</p>
+    <div class="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped bg-info" style="width: 10%"></div>
+</div>
+    <p class="d-inline-flex gap-1 mt-3">
+     <button type="button" class="btn text-warning" disabled data-bs-toggle="button">Vào kì thi</button>
+    </p>
+  </div>
+</div>
+</div>
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center mt-3">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+</article>
+</div>
 </body>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 </html>
